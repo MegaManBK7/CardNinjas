3546037b6597f623b7708351bf568c2fb5c4d6cb
Neal Kaviratna <Nkaviratna@gatech.edu>
2015-11-06 01:35:17 -0500

    Menu near full functionality SPAGHETTI W/ CHANCE OF MBALLS

---
+++ Assets/Scripts/Enemies/FullScreenToggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.Scripts.Util;
+using UnityEngine.EventSystems;
+
+public class FullScreenToggle : MonoBehaviour {
+
+	public Toggle toggle;
+
+	// Use this for initialization
+	void Start () {
+		toggle.onValueChanged.AddListener (delegate {FullscreenUpdate ();});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void FullscreenUpdate() {
+		Debug.Log("go");
+		Screen.fullScreen = toggle.isOn;
+	}
+
+
+}
---
+++ Assets/Scripts/UI/SliderFix.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.Scripts.Util;
+using UnityEngine.EventSystems;
+
+public class SliderFix : MonoBehaviour {
+
+	public Slider slider;
+	public SoundPlayer sp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) Navigate(CustomInput.UserInput.Right);
+		if (Input.GetKey(KeyCode.LeftArrow)) Navigate(CustomInput.UserInput.Left);
+	}
+
+	#region NAVIGATION
+	private void Navigate(CustomInput.UserInput direction)
+	{
+		if ( EventSystem.current.currentSelectedGameObject == this.gameObject) {
+			switch(direction)
+			{
+			case CustomInput.UserInput.Left:
+				slider.value -= .01f;
+				break;
+			case CustomInput.UserInput.Right:
+				slider.value += .01f;
+				break;
+			}
+			if (sp) sp.SetVolume(slider.value);
+		}
+	}
+	#endregion
+}
