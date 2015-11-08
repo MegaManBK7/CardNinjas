81b49c4cbd8ca53a751ad6ca927d5eb2b43be1ca
Eric Cook <jcook83@gatech.edu>
2015-11-08 13:44:27 -0500

    Multiplayer Battle 99% done. One bug still
    Somehow player 2 draw 16 cards every time instead of 8

--- Assets/Scripts/UI/CardSelectorMultiplayer.cs
+++ Assets/Scripts/UI/CardSelectorMultiplayer.cs
@@ -27,7 +27,6 @@ namespace Assets.Scripts.UI
         private Image[] finalButtons;
 
         private static Image[] xboxButtons;
-        private static Text[] keyboardButtons;
 
         [SerializeField]
         private List<Sprite> xboxButtonSprites;
@@ -48,7 +47,6 @@ namespace Assets.Scripts.UI
             //should find players provided they are named in the fashion: "Player 1" or "Player 42"
             thisPlayerIndex = ++playerIndex;
             player = GameObject.Find("Player " + playerIndex).GetComponent<Player.Player>();
-
             deck = player.Deck;
             selectionOptions = new List<Card>();
             selectedCards = new List<Card>();
@@ -57,14 +55,14 @@ namespace Assets.Scripts.UI
             selectionButtons = new GameObject[NUM_SELECTIONS];
             finalButtons = new Image[MAX_SELECTIONS];
             GameObject[] gos = GameObject.FindGameObjectsWithTag("Selection").OrderBy(go => go.name).ToArray();
-            for (int i = 0; i < gos.Length; i++)
+            for (int i = 0; i < NUM_SELECTIONS; i++)
             {
                 gos[i].tag = "Selection " + thisPlayerIndex.ToString();
                 selectionButtons[i] = gos[i];
             }
             
             gos = GameObject.FindGameObjectsWithTag("Final").OrderBy(go => go.name).ToArray();
-            for (int i = 0; i < gos.Length; i++)
+            for (int i = 0; i < MAX_SELECTIONS; i++)
             {
                 gos[i].tag = "Final " + thisPlayerIndex.ToString();
                 finalButtons[i] = gos[i].GetComponent<Image>();
@@ -74,7 +72,7 @@ namespace Assets.Scripts.UI
             {
                 xboxButtons = new Image[xboxButtonSprites.Count];
                 gos = GameObject.FindGameObjectsWithTag("Xbox Button").OrderBy(go => go.name).ToArray();
-                for (int i = 0; i < gos.Length; i++)
+                for (int i = 0; i < NUM_SELECTIONS * 2; i++)
                 {
                     xboxButtons[i] = gos[i].GetComponent<Image>();
                 }
@@ -87,45 +85,15 @@ namespace Assets.Scripts.UI
                 xboxButtons[5].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard5, thisPlayerIndex)));
                 xboxButtons[6].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard6, thisPlayerIndex)));
                 xboxButtons[7].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard7, thisPlayerIndex)));
-
-                //xboxButtons[8].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard0, thisPlayerIndex)));
-                //xboxButtons[9].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard1, thisPlayerIndex)));
-                //xboxButtons[10].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard2, thisPlayerIndex)));
-                //xboxButtons[11].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard3, thisPlayerIndex)));
-                //xboxButtons[12].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard4, thisPlayerIndex)));
-                //xboxButtons[13].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard5, thisPlayerIndex)));
-                //xboxButtons[14].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard6, thisPlayerIndex)));
-                //xboxButtons[15].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard7, thisPlayerIndex)));
-            }
-
-            if (keyboardButtons == null)
-            {
-                keyboardButtons = new Text[8];
-                gos = GameObject.FindGameObjectsWithTag("Keyboard Button").OrderBy(go => go.name).ToArray();
-                for (int i = 0; i < gos.Length; i++)
-                {
-                    keyboardButtons[i] = gos[i].GetComponent<Text>();
-                }
-
-                keyboardButtons[0].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard0, thisPlayerIndex).ToString();
-                keyboardButtons[1].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard1, thisPlayerIndex).ToString();
-                keyboardButtons[2].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard2, thisPlayerIndex).ToString();
-                keyboardButtons[3].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard3, thisPlayerIndex).ToString();
-                keyboardButtons[4].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard4, thisPlayerIndex).ToString();
-                keyboardButtons[5].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard5, thisPlayerIndex).ToString();
-                keyboardButtons[6].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard6, thisPlayerIndex).ToString();
-                keyboardButtons[7].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard7, thisPlayerIndex).ToString();
-
-                /*
-                keyboardButtons[8].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard0, thisPlayerIndex).ToString();
-                keyboardButtons[9].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard1, thisPlayerIndex).ToString();
-                keyboardButtons[10].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard2, thisPlayerIndex).ToString();
-                keyboardButtons[11].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard3, thisPlayerIndex).ToString();
-                keyboardButtons[12].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard4, thisPlayerIndex).ToString();
-                keyboardButtons[13].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard5, thisPlayerIndex).ToString();
-                keyboardButtons[14].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard6, thisPlayerIndex).ToString();
-                keyboardButtons[15].text = CustomInput.keyBoardKey(CustomInput.UserInput.PickCard7, thisPlayerIndex).ToString();
-                */
+                
+                xboxButtons[8].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard0, thisPlayerIndex)));
+                xboxButtons[9].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard1, thisPlayerIndex)));
+                xboxButtons[10].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard2, thisPlayerIndex)));
+                xboxButtons[11].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard3, thisPlayerIndex)));
+                xboxButtons[12].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard4, thisPlayerIndex)));
+                xboxButtons[13].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard5, thisPlayerIndex)));
+                xboxButtons[14].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard6, thisPlayerIndex)));
+                xboxButtons[15].sprite = xboxButtonSprites.Find(x => x.name.Contains(CustomInput.gamePadButton(CustomInput.UserInput.PickCard7, thisPlayerIndex)));
             }
 
             DrawPossibleSelections();
@@ -142,10 +110,10 @@ namespace Assets.Scripts.UI
                     DrawPossibleSelections();
                 }
 
-                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard0, thisPlayerIndex)) SelectCard(0);
-                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard1, thisPlayerIndex)) SelectCard(1);
-                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard2, thisPlayerIndex)) SelectCard(2);
-                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard3, thisPlayerIndex)) SelectCard(3);
+                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard0, 1)) SelectCard(0);
+                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard1, 1)) SelectCard(1);
+                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard2, 1)) SelectCard(2);
+                if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard3, 1)) SelectCard(3);
                 if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard4, thisPlayerIndex)) SelectCard(4);
                 if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard5, thisPlayerIndex)) SelectCard(5);
                 if (CustomInput.BoolFreshPress(CustomInput.UserInput.PickCard6, thisPlayerIndex)) SelectCard(6);
@@ -171,6 +139,7 @@ namespace Assets.Scripts.UI
 
         public void SelectCard(int index)
         {
+            if (selectionOptions == null || index >= selectionOptions.Count) return;
             if (finalMap.Contains(index))
             {
                 selectedCards.Remove(selectionOptions[index]);
@@ -238,7 +207,7 @@ namespace Assets.Scripts.UI
                     finalButtons[i].color = CustomColor.Convert255(255, 255, 255);
                 }
             }
-            if(totalSelections >= MAX_SELECTIONS)
+            if (totalSelections >= MAX_SELECTIONS * 2 || selectionOptions == null || totalSelections >= selectionOptions.Count)
             {
                 if (CardsSelected != null) CardsSelected();
             }
