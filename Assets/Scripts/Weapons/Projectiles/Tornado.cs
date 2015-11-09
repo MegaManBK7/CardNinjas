0d8e1cc6c5e171303afc6497a1035d2d54e6024d
Jonathan Hunter <hunterjonathant@gmail.com>
2015-11-08 19:14:14 -0500

    fixed hitbox, started adding Tornado

---
+++ Assets/Scripts/Weapons/Projectiles/Tornado.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Weapons.Projectiles
+{
+    class Tornado : Hitbox
+    {
+        public override void OnTriggerEnter(Collider collider)
+        {
+            Hitbox h = collider.gameObject.GetComponent<Hitbox>();
+            if (h != null)
+            {
+                Physics.IgnoreCollision(this.GetComponent<Collider>(), collider);
+                return;
+            }
+            if (collider.tag == "Enemy")
+            {
+                Physics.IgnoreCollision(this.GetComponent<Collider>(), collider);
+                return;
+            }
+        }
+    }
+}
