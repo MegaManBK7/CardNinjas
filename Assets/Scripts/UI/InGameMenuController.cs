9205e5f8b61f756a3b99df5dd27ab2e722803c68
Neal Kaviratna <Nkaviratna@gatech.edu>
2015-11-09 19:12:07 -0500

    In Game menu logic added + got submit working with custom input

---
+++ Assets/Scripts/UI/InGameMenuController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InGameMenuController : MonoBehaviour {
+
+	public GameObject Pause;
+	public GameObject PauseSelect;
+
+	public GameObject Win;
+	public GameObject WinSelect;
+
+	public GameObject Lose;
+	public GameObject LoseSelect;
+
+	public GameObject P1Win;
+	public GameObject P1WinSelect;
+
+	public GameObject P2Win;
+	public GameObject P2WinSelect;
+
+	private EventSystem es = EventSystem.current;
+	private UIHideBehaviour hideBehaviour;
+
+	public void Start() {
+		hideBehaviour = GetComponent<UIHideBehaviour>();
+	}
+
+	public void BringUpP2Win() {
+		Win.SetActive(false);
+		Lose.SetActive(false);
+		P1Win.SetActive(false);
+		Pause.SetActive(false);
+		P2Win.SetActive(true);
+		
+		hideBehaviour.OnScreen = true;
+		
+		es.SetSelectedGameObject(P2WinSelect);
+	}
+
+	public void BringUpP1Win() {
+		Win.SetActive(false);
+		Lose.SetActive(false);
+		Pause.SetActive(false);
+		P2Win.SetActive(false);
+		P1Win.SetActive(true);
+		
+		hideBehaviour.OnScreen = true;
+		
+		es.SetSelectedGameObject(P1WinSelect);
+	}
+
+	public void BringUpLose() {
+		Win.SetActive(false);
+		Pause.SetActive(false);
+		P1Win.SetActive(false);
+		P2Win.SetActive(false);
+		Lose.SetActive(true);
+		
+		hideBehaviour.OnScreen = true;
+		
+		es.SetSelectedGameObject(LoseSelect);
+	}
+
+	public void BringUpWin() {
+		Pause.SetActive(false);
+		Lose.SetActive(false);
+		P1Win.SetActive(false);
+		P2Win.SetActive(false);
+		Win.SetActive(true);
+		
+		hideBehaviour.OnScreen = true;
+		
+		es.SetSelectedGameObject(WinSelect);
+	}
+
+	public void BringUpPause() {
+		Win.SetActive(false);
+		Lose.SetActive(false);
+		P1Win.SetActive(false);
+		P2Win.SetActive(false);
+		Pause.SetActive(true);
+
+		hideBehaviour.OnScreen = true;
+		
+		es.SetSelectedGameObject(PauseSelect);
+	}
+
+	public void DismissDialog() {
+		hideBehaviour.OnScreen = false;
+	}
+}
