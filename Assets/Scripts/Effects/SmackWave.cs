09373a8d7f8ad1b6b282463e012f8468fe1ba9f1
Spex130 <Gamerguy56@bellsouth.net>
2015-11-10 23:44:33 -0500

    Big 'ol change dump.
    Karasu's materials have been edited to make him less obnoxiously shiny.
    
    5 Effects:
    - 2 Circular shockwaves: "Smackwave1 & Smackwave2"
    - 2 animated Slash  prefabs taken from the old slash models.
    - Dust cloud emitter
    
    Self-Destroy script so effect animations can kill their gameObject when
    they finish.
    
    ALSO, Oni Armor enemy AI and Test Oni Wave.

---
+++ Assets/Scripts/Effects/SmackWave.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmackWave : MonoBehaviour {
+
+	[SerializeField]
+	public float scaleFactor = 1;
+
+	[SerializeField]
+	public float deathThreshold = .01f;
+
+	[SerializeField]
+	public bool terminateSelf = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = new Vector3(this.transform.localScale.x + scaleFactor, this.transform.localScale.y - scaleFactor, this.transform.localScale.z + scaleFactor);
+
+		if(this.transform.localScale.y <= deathThreshold && terminateSelf)
+		{
+			Destroy(this.gameObject);
+
+		}
+    }
+}
